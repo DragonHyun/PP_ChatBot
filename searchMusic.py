c5709e43c4647b48da6d5de4af1f124d43d0705e
DragonHyun <dragonhyunlee@gmail.com>
2019-12-03 17:20:28 +0900

    update searchMusic.py

--- searchMusic.py
+++ searchMusic.py
@@ -17,5 +17,7 @@ soup = BeautifulSoup(html, 'html.parser')
 musicName = soup.findAll('span', {'class':'t_point'})
 artists = soup.findAll('a', {'class':'artist ellipsis'})
 
-for i in range(4):
-    print(musicName[i].text + " - " + artists[i].text)
+for i in range(len(musicName)):
+    musicName = musicName[i].text.strip().split('\n')[0]
+    artists = artists[i].text.strip().split('\n')[0]
+    print('{0:3d}. {1} - {2}'.format(i + 1, musicName, artists))
