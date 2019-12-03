e2fd434cb8efc837b2449706d02156033a992eba
DragonHyun <dragonhyunlee@gmail.com>
2019-12-03 18:01:05 +0900

    update searchMusic.py

--- searchMusic.py
+++ searchMusic.py
@@ -13,9 +13,10 @@ html = request.text
 soup = BeautifulSoup(html, 'html.parser')
 songs = soup.select('#body-content > div.search_song > div.search_result_detail > div.music-list-wrap > table > tbody > tr')
 
-
+for song in songs:
+    title = song.find('td',{'class':'info'}).find('a',{'class':'title ellipsis'})
+    print(title.strip())
+    
 #musicName = soup.select('span', {'class':'t_point'})
 #artists = soup.findAll('a', {'class':'artist ellipsis'})
 #test = soup.findAll('td', {'class':'info'})
-
-print(songs)
