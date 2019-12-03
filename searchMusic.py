fa154f0b0f4fcb814b5428b738df59748775951d
DragonHyun <dragonhyunlee@gmail.com>
2019-12-03 17:58:08 +0900

    update searchMusic.py

--- searchMusic.py
+++ searchMusic.py
@@ -6,14 +6,16 @@ from bs4 import BeautifulSoup
 musicName = 'despacito'
 url = 'https://www.genie.co.kr/search/searchMain?query='
 url = url + musicName
-print(url)
+
 request = requests.get(url)
 html = request.text
 
 soup = BeautifulSoup(html, 'html.parser')
+songs = soup.select('#body-content > div.search_song > div.search_result_detail > div.music-list-wrap > table > tbody > tr')
 
-musicName = soup.findAll('span', {'class':'t_point'})
+
+#musicName = soup.select('span', {'class':'t_point'})
 #artists = soup.findAll('a', {'class':'artist ellipsis'})
 #test = soup.findAll('td', {'class':'info'})
 
-print(musicName)
+print(songs)
