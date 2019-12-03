11aa6ab233352b65c6549fcd3274f3f36988ce48
DragonHyun <dragonhyunlee@gmail.com>
2019-12-03 17:40:03 +0900

    update searchMusic.py

--- searchMusic.py
+++ searchMusic.py
@@ -9,12 +9,12 @@ url = 'https://www.genie.co.kr/search/searchMain?query='
 url = url + musicName
 
 request = requests.get(url)
-print(request)
 html = request.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-#musicName = soup.findAll('span', {'class':'t_point'})
+musicName = soup.findAll('span', {'class':'t_point'})
 #artists = soup.findAll('a', {'class':'artist ellipsis'})
 #test = soup.findAll('td', {'class':'info'})
-#print(test)
+
+print(musicName)
