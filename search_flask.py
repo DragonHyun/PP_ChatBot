fc686245a949513ea18d8cac3c267959204fa686
DragonHyun <dragonhyunlee@gmail.com>
2019-12-05 14:22:11 +0900

    update search_flask.py

--- search_flask.py
+++ search_flask.py
@@ -4,6 +4,8 @@ from flask import Flask, request, jsonify
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import urllib
+import requests
+#import json
 
 ERROR_MESSAGE = '네트워크 접속에 문제가 발생하였습니다. 잠시 후 다시 시도해주세요.'
 
@@ -15,13 +17,14 @@ def searchMusic():
     req = request.get_json()
     
     musicName = req["action"]["detailParams"]["sys_music_name"]["value"]
+    #musicName = str(urllib.parse.quote(musicName))
     
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     url = 'https://www.genie.co.kr/search/searchMain?query='
     url = url + musicName
 
-    request = requests.get(url, headers = headers)
-    html = request.text
+    req = requests.get(url, headers = headers)
+    html = req.text
 
     soup = BeautifulSoup(html, 'html.parser')
     songs = soup.select('#body-content > div.search_song > div.search_result_detail > div > table > tbody > tr')
@@ -47,7 +50,6 @@ def searchMusic():
             ]
         }
     }
-
     return jsonify(res)
 
 if __name__ == '__main__':
