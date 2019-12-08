5fcbd5f70640b9910c077a0010d1f8901cad9913
DragonHyun <dragonhyunlee@gmail.com>
2019-12-08 19:34:43 +0900

    update search_flask.py

--- search_flask.py
+++ search_flask.py
@@ -11,6 +11,26 @@ ERROR_MESSAGE = '네트워크 접속에 문제가 발생하였습니다. 잠시 
 
 app = Flask(__name__)
 
+@app.route('/searchYoutube', methods=['POST'])
+def test():
+    req = request.get_json()
+    ans = req['action']['clientExtra']['title'] + " " + req['action']['clientExtra']['artist']
+    res = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": ans
+                        }
+                    }
+            ]
+        }
+    }
+
+    return jsonify(res)
+
+
 @app.route('/searchMusic', methods=['POST'])
 def searchMusic():
     
@@ -57,25 +77,53 @@ def searchMusic():
         "template": {
             "outputs": [
                 {
-                    "listCard": {
-                        "header": {
-                            "title": "검색 결과"
-                        },
+                    "carousel": {
+                        "type": "basicCard",
                         "items": [
                             {
                                 "title": titleList[0],
                                 "description": artistList[0],
-                                "imageUrl": imgList[0]
+                                "thumbnail": {
+                                    "imageUrl": imgList[0]
+                                },
+                                "buttons": [
+                                    {
+                                        "action": "block",
+                                        "label": "추가",
+                                        "blockId": "5dec8f6b92690d0001fbe3f4",
+                                        "extra": {'title': titleList[0], 'artist': artistList[0]}
+                                    }
+                                ]
                             },
                             {
                                 "title": titleList[1],
                                 "description": artistList[1],
-                                "imageUrl": imgList[1]
+                                "thumbnail": {
+                                    "imageUrl": imgList[1]
+                                },
+                                "buttons": [
+                                    {
+                                        "action": "block",
+                                        "label": "추가",
+                                        "blockId": "5dec8f6b92690d0001fbe3f4",
+                                        "extra": {'title': titleList[1], 'artist': artistList[1]}
+                                    }
+                                ]
                             },
                             {
                                 "title": titleList[2],
                                 "description": artistList[2],
-                                "imageUrl": imgList[2]
+                                "thumbnail": {
+                                    "imageUrl": imgList[2]
+                                },
+                                "buttons": [
+                                    {
+                                        "action": "block",
+                                        "label": "추가",
+                                        "blockId": "5dec8f6b92690d0001fbe3f4",
+                                        "extra": {'title': titleList[2], 'artist': artistList[2]}
+                                    }
+                                ]
                             }
                         ]
                     }
